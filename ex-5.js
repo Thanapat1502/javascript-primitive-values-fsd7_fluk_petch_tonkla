e1fa3aba67d61b38b07abea59878ef4d9158c651
Tonkla <Apisit378819@gmail.com>
2024-09-30 14:26:30 +0700

    The test is complete

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,9 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+
+let productPrice = 27000;
+let discount = 25/100;
+let totalprice = productPrice * discount;
+totalprice = productPrice - totalprice;
+console.log(totalprice); 
